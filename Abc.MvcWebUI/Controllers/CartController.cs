090797433b97a326bde4a2e490f515772a5eb43b
sadikturan <sadik_turan@hotmail.com>
2017-09-19 22:07:56 +0300

    Ders5 güncelleme

---
+++ Abc.MvcWebUI/Controllers/CartController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Abc.MvcWebUI.Entity;
+using Abc.MvcWebUI.Models;
+
+namespace Abc.MvcWebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private DataContext db = new DataContext();
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View(GetCart());
+        }
+
+        public ActionResult AddToCart(int Id)
+        {
+            var product = db.Products.FirstOrDefault(i => i.Id == Id);
+
+            if (product != null)
+            {
+                GetCart().AddProduct(product,1);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromCart(int Id)
+        {
+            var product = db.Products.FirstOrDefault(i => i.Id == Id);
+
+            if (product != null)
+            {
+                GetCart().DeleteProduct(product);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public Cart GetCart()
+        {
+            var cart = (Cart)Session["Cart"];
+
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+
+        public PartialViewResult Summary()
+        {
+            return PartialView(GetCart());
+        }
+    }
+}
---
+++ Abc.MvcWebUI/Models/Cart.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Abc.MvcWebUI.Entity;
+
+namespace Abc.MvcWebUI.Models
+{
+    public class Cart
+    {
+        private List<CartLine> _cardLines = new List<CartLine>();
+
+        public List<CartLine> CartLines
+        {
+            get { return _cardLines; }
+        }
+
+        public void AddProduct(Product product, int quantity)
+        {
+            var line = _cardLines.FirstOrDefault(i => i.Product.Id == product.Id);
+            if (line == null)
+            {
+                _cardLines.Add(new CartLine() { Product = product, Quantity = quantity });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            _cardLines.RemoveAll(i => i.Product.Id == product.Id);
+        }
+
+        public double Total()
+        {
+            return _cardLines.Sum(i => i.Product.Price * i.Quantity);
+        }
+
+        public void Clear()
+        {
+            _cardLines.Clear();
+        }
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
